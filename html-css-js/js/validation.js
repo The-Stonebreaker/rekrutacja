a1fca8cf4c522d99363e50c2f8f416c7339d4da4
The-Stonebreaker <98124442+The-Stonebreaker@users.noreply.github.com>
2022-10-22 17:05:47 +0100

    Update validation.js

--- html-css-js/js/validation.js
+++ html-css-js/js/validation.js
@@ -9,3 +9,23 @@
     Powodzenia!
     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 */
+
+function validateForm() {
+  let first_name = document.forms["myForm"]["first-name"].value;
+  let last_name = document.forms["myForm"]["last-name"].value;
+  let email = document.forms["myForm"]["email"].value;
+  
+  
+  if (first_name == "" || first_name.length<3) {
+    alert("Niestety, ale nie wiemy kim jesteś!\nMusisz podać swoje imie");
+    return false;
+  }
+  if (last_name == "" || last_name.length<3) {
+    alert("Niestety, ale nie wiemy kim jesteś!\nMusisz podać swoje nazwisko");
+    return false;
+  }
+  if (email == "") {
+    alert("Jakoś musimy się z tobą skontaktować!\nPodaj adres e-mail");
+    return false;
+  }
+}
